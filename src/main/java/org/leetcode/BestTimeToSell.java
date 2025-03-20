7d82dfb525efe4e2eab3d1e3f7be02b318ec4668
okiyo <okiyostephen@gmail.com>
2025-03-20 17:00:19 +0300

    given an array return the max profit one can get when iterating.

---
+++ src/main/java/org/leetcode/BestTimeToSell.java
@@ -0,0 +1,21 @@
+package org.leetcode;
+
+public class BestTimeToSell {
+
+
+    public static void main(String[] args) {
+
+        Integer maxProfit = 0;
+
+        int [] sales = {3,4,1,6,11,8,6,10};
+
+        for(int i = 0; i < sales.length - 1; i++) {
+            if(sales[i +1 ] > sales[i]) {
+                maxProfit += sales[i+1] - sales[i];
+            }
+
+        }
+
+        System.out.println(maxProfit);
+    }
+}
