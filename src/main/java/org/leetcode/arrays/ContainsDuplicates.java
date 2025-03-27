ecc8e85d3529f041ddae1a40b7a92b796ce0feb8
okiyo <okiyostephen@gmail.com>
2025-03-27 11:08:36 +0300

    improve ContainsDuplicates implementation.

--- src/main/java/org/leetcode/arrays/ContainsDuplicates.java
+++ src/main/java/org/leetcode/arrays/ContainsDuplicates.java
@@ -8,9 +8,15 @@ public class ContainsDuplicates {
 
     public static void main(String[] args) {
 
+        int [] nums = {1,2,3,4,5,6,7,8,9};
+
+        boolean duplicates= containsDuplicate(nums);
+        System.out.println(duplicates);
+
+
     }
 
-    public boolean containsDuplicate(int[] nums) {
+    public static boolean containsDuplicate(int[] nums) {
         if (nums == null || nums.length == 0) return false;
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
