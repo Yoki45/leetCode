cd8c3bd191b19997a344acde9ae628b15ca2e044
okiyo <okiyostephen@gmail.com>
2025-04-02 14:54:32 +0300

    three sum.

--- src/main/java/org/leetcode/arrays/ThreeSum.java
+++ src/main/java/org/leetcode/arrays/ThreeSum.java
@@ -35,7 +35,10 @@ public class ThreeSum {
                 int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0) {
                     res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while (left < right && nums[left] == nums[left + 1]) {}
                     left++;
+                    while (left < right && nums[right] == nums[right - 1]) {}
                     right--;
 
                 } else if (sum < 0) {
